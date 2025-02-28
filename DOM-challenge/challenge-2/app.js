a48e95949dc0ed0ef680055b94653617f700ae5f
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2025-02-28 20:54:25 +0530

    challenge 2 complete

--- DOM-challenge/challenge-2/app.js
+++ DOM-challenge/challenge-2/app.js
@@ -1,3 +1,27 @@
 /**
  * Write your challenge solution here
  */
+let button=document.querySelectorAll("button")
+let text=document.getElementById('mainHeading')
+
+button.forEach((btn)=>{
+     btn.addEventListener('click',()=>{
+        switch(btn.innerText.toLowerCase()){
+            case "red" :
+                text.style.color="#e74c3c"
+                break;
+            case "green" :
+                text.style.color="#2ecc71"
+                break;
+            case "blue" :
+                text.style.color="#3498db"
+                break;
+            case "purple" :
+                text.style.color="#9b59b6"
+                break;
+            case "reset":
+                text.style.color="#34495e"
+                break;
+        }
+     })
+})
