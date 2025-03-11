ebf0e30b5c4382cdd5ea17f371bbd03a44be28ad
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2025-03-11 23:34:14 +0530

    debouncing

--- polyfills/debouncing.js
+++ polyfills/debouncing.js
@@ -24,4 +24,4 @@ function greet(name){
 const callFunc=debounce(()=>greet("angshuman"), 1000); 
 callFunc() // when its fist call with arguments the myId is undefined so line no 8 is not executable then line no 10 calling with the delay
 callFunc() // then second call with this arguments myId hold the reference of the previous call then line 8 excecute and clear the previous reference and the line 10 excute with fn 
-
+callFunc() // last time only function call previous all the are deleted
